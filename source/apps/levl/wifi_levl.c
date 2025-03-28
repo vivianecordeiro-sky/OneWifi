c225f247b34df9217c363d7fcd2521b85e157d10
Samyuktha T K <samyuktha_karthikeyan@comcast.com>
2025-03-28 14:45:14 +0530

    RDKB-59288 Overall Memory usage is increasing continuously (#230)
    SHARMAN-3237 Overall Memory usage is increasing continuously
    
    Reason for change: To free the memory after allocation
    
    Test Procedure: Flash the build in HUB6 and check for increase in the
                    memory
    Priority: P0
    
    Risks: Low

--- source/apps/levl/wifi_levl.c
+++ source/apps/levl/wifi_levl.c
@@ -810,6 +810,7 @@ void push_radio_temperature_request_to_monitor_queue(wifi_mon_stats_request_stat
     data->u.mon_stats_config.data_type = mon_stats_type_radio_temperature;
     data->u.mon_stats_config.args.radio_index = radio_index;
     push_event_to_monitor_queue(data, wifi_event_monitor_data_collection_config, &route);
+    free(data);
 }
 
 int webconfig_event_levl(wifi_app_t *apps, wifi_event_subtype_t sub_type, void *data)
