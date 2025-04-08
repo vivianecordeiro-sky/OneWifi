ea26c23182d17cba2e034bf6e757b92815e79d93
poornakumar-pk <poornakumar_mezhiselvam@comcast.com>
2025-04-08 10:36:10 +0530

    RDKB-58764 [Grey list] Gateway Failure in adding device to Grey listing table  (#250)
    RDKB-58764 : [Grey list] Gateway Failure in adding device to Grey listing table
    
    Impacted Platforms: All OneWifi platforms
    Reason for change: mac filter was not enabled for hotspotvaps by default
    Test Procedure: Test greylist and check whether the maclist have been properly added in maclist table
    
    Priority: P1
    
    Risks: Low
    Signed-off-by:poornakumar_mezhiselvam@comcast.com
    Co-authored-by: Sathish Kumar Gnanasekaran <gsathish86@gmail.com>

--- source/db/wifi_db.c
+++ source/db/wifi_db.c
@@ -349,6 +349,9 @@ static int init_vap_config_default(int vap_index, wifi_vap_info_t *config,
         if (isVapMeshBackhaul(vap_index)) {
             cfg.u.bss_info.mac_filter_enable = true;
             cfg.u.bss_info.mac_filter_mode = wifi_mac_filter_mode_white_list;
+        } else if (isVapHotspot(vap_index)) {
+            cfg.u.bss_info.mac_filter_enable = true;
+            cfg.u.bss_info.mac_filter_mode = wifi_mac_filter_mode_black_list;
         } else {
             cfg.u.bss_info.mac_filter_enable = false;
         }
