d7360849374a81af42999b342061b6b44f6d8c07
Jayanthrajan <jayanth.rajan@sky.uk>
2025-04-10 12:33:38 +0100

    RDKB-59277 : Push Station Manager code to WiFiStaManager repo. (#257)
    Reason for change: Script change to pull stamgr as a seperate target
    Test Procedure: Station manger code should checkout and build properly.
    Risks: Low
    
    Signed-off-by: Jayanth Rajan <jayanth.rajan@sky.uk>

---
+++ build/linux/stamgr_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ONEWIFI_DIR=$(pwd)
+STA_MGR_DIR="$(pwd)/../WiFiStaManager"
+
+cd ..
+git clone -b 25Q2_sprint https://gerrit.teamccp.com/rdk/rdkb/components/opensource/ccsp/WiFiStaManager/generic WiFiStaManager
+if [ -d "$STA_MGR_DIR" ]; then
+       echo "copying sta_mgr..."
+       cp -r $STA_MGR_DIR/* $ONEWIFI_DIR/../../source/apps/sta_mgr
+fi
+
+#return back to initial directory
+cd $ONEWIFI_DIR
