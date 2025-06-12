0640c7e37d6aee2ee1089e92a3bb5a3597854a17
PalakshaOS <pali974059@gmail.com>
2025-06-12 16:17:11 +0100

    SHARMAN-3535 : WPS disable config failed to persist after reboot (#328) (#393)
    Reason for change:  wifidb_init_vap_config_default called after Db read causing config to go back to default
    Test Procedure: NA
    Risks: Low
    
    Signed-off-by: Palaksha Gowda <palaksha.gowda@sky.uk>

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -4538,7 +4538,7 @@ void wifidb_vap_config_correction(wifi_vap_info_map_t *l_vap_map_param)
 
         if (isVapPrivate(vap_config->vap_index) &&
             is_sec_mode_personal(vap_config->u.bss_info.security.mode)) {
-#ifdef FEATURE_SUPPORT_WPS
+#if defined(FEATURE_SUPPORT_WPS) &&  !defined(_SR213_PRODUCT_REQ_)
             if (vap_config->u.bss_info.wps.enable == false) {
                 vap_config->u.bss_info.wps.enable = true;
                 wifi_util_info_print(WIFI_DB, "%s:%d: force wps enabled for private_vap:%d\r\n",
