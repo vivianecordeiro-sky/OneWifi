26ccba1991f6b28a31eeb4fc4e8abb9cbd5ee7c9
Rakhil P E <117166043+rakhilpe@users.noreply.github.com>
2025-08-07 07:31:33 +0530

    RDKB-60610: OpenWRT: Channel change related kernel patch issue (#525)
    Reason for change: Re enabled the kernel patch related to
    Channel change after making change not to create .rej file if the
    patch is already applied.
    Risks: None
    Priority: P1
    
    Signed-off-by: Rakhil P E <rakhilpe001@gmail.com>

--- build/openwrt/setup.sh
+++ build/openwrt/setup.sh
@@ -60,5 +60,7 @@ cp -r build/openwrt/avro ../../libs/.
 
 #Applying kernel patch from openwrt root directory
 cd $OPENWRT_ROOT
-#patch --forward -p1 < $KERNEL_PATCH_DIR/0001-BPIR4_Enable_Beacon_Frame_Subscription.patch
+if patch --dry-run --forward -p1 < $KERNEL_PATCH_DIR/0001-BPIR4_Enable_Beacon_Frame_Subscription.patch; then
+        patch --forward -p1 < $KERNEL_PATCH_DIR/0001-BPIR4_Enable_Beacon_Frame_Subscription.patch
+fi
 cd $ONEWIFI_DIR
